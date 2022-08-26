56b01680535a03454e76fb54cb2f107829766927
sher304 <esherovshermat@gmail.com>
2022-08-26 11:58:08 +0600

    :rocket:Done Project

---
+++ Ecommerce/Model/CategoryModel.swift
@@ -0,0 +1,15 @@
+//
+//  CategoryModel.swift
+//  Ecommerce
+//
+//  Created by Шермат Эшеров on 26/8/22.
+//
+
+import Foundation
+import UIKit
+
+struct CategoryModel{
+    let title: String?
+    let image: UIImage?
+    let didSelected: Bool?
+}
